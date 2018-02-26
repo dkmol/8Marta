dc43dab977e999ba9657a95768f70e6afd6945be
dkmol <34780005+dkmol@users.noreply.github.com>
2018-02-26 15:59:16 +0300

    renamed some functions

--- Code/March8/March8.ino
+++ Code/March8/March8.ino
@@ -26,34 +26,42 @@ Adafruit_NeoPixel LEDS = Adafruit_NeoPixel(PIXEL_NUM, LEDS_PIN, NEO_GRB + NEO_KH
 
 
 void setup()
-{ 
+{
   LEDS.begin();
-  for(byte i=0; i< PIXEL_NUM; i++)
+  for (byte i = 0; i < PIXEL_NUM; i++)
     LEDS.setPixelColor(i, BACKGROUND_COLOUR);
   LEDS.show();
 }
 //-----------------------------------
 void loop()
 {
-  for(byte i=0; i < 3; i++)
-    LEDS_1();
-  for(byte i=0; i < 3; i++)
-    LEDS_2();
-  for(byte i=0; i < 3; i++)
-    LEDS_3();
-  for(byte i=0; i < 10; i++)
+
+  for (byte i = 0; i < 3; i++)
+    spotLight();
+
+  for (byte i = 0; i < 3; i++)
+    cometLight();
+
+  for (byte i = 0; i < 3; i++)
+    flash();
+
+  for (byte i = 0; i < 3; i++)
+    snake();
+
+  for (byte i = 0; i < 10; i++)
     LEDS_4();
+
 }
 //-----------------------------------
-void LEDS_1()
+void spotLight()
 {
   const unsigned int ShowDelay = 50;
 
-  for(byte i=0; i< PIXEL_NUM; i++)
+  for (byte i = 0; i < PIXEL_NUM; i++)
     LEDS.setPixelColor(i, BACKGROUND_COLOUR);
   LEDS.show();
-  
-  for(byte i=0; i< PIXEL_NUM; i++)
+
+  for (byte i = 0; i < PIXEL_NUM; i++)
   {
     LEDS.setPixelColor(i, 150, 58, 255); //цвет R, G, B
     LEDS.show();
@@ -62,74 +70,74 @@ void LEDS_1()
   }
 }
 //-----------------------------------
-void LEDS_2()
+void cometLight()
 {
   const unsigned int ShowDelay = 75;
 
-  for(byte i=0; i < (PIXEL_NUM / 2); i++)
+  for (byte i = 0; i < (PIXEL_NUM / 2); i++)
   {
     LEDS.setPixelColor(i, 150, 58, 255); //цвет R, G, B
     if (i >= 1)
-      LEDS.setPixelColor(i-1, PALE1_COLOUR); //цвет R, G, B
+      LEDS.setPixelColor(i - 1, PALE1_COLOUR); //цвет R, G, B
     else
     {
       LEDS.setPixelColor(PIXEL_IN_MODULE, PALE1_COLOUR); //цвет R, G, B
       LEDS.setPixelColor(PIXEL_IN_MODULE + 1, PALE2_COLOUR);
     }
     if (i >= 2)
-      LEDS.setPixelColor(i-2, PALE2_COLOUR); //цвет R, G, B
+      LEDS.setPixelColor(i - 2, PALE2_COLOUR); //цвет R, G, B
     else
       LEDS.setPixelColor(PIXEL_IN_MODULE, PALE2_COLOUR);
-      
+
     LEDS.show();
     delay(ShowDelay);
     LEDS.setPixelColor(i, BACKGROUND_COLOUR);
     if (i >= 1)
-      LEDS.setPixelColor(i-1, BACKGROUND_COLOUR); //цвет R, G, B
+      LEDS.setPixelColor(i - 1, BACKGROUND_COLOUR); //цвет R, G, B
     else
     {
       LEDS.setPixelColor(PIXEL_IN_MODULE, BACKGROUND_COLOUR); //цвет R, G, B
       LEDS.setPixelColor(PIXEL_IN_MODULE + 1, BACKGROUND_COLOUR);
     }
     if (i >= 2)
-      LEDS.setPixelColor(i-2, BACKGROUND_COLOUR); //цвет R, G, B
+      LEDS.setPixelColor(i - 2, BACKGROUND_COLOUR); //цвет R, G, B
     else
       LEDS.setPixelColor(PIXEL_IN_MODULE, BACKGROUND_COLOUR);
   }
 
-  for(byte i = (PIXEL_NUM - 1); i >= (PIXEL_NUM / 2); i--)
+  for (byte i = (PIXEL_NUM - 1); i >= (PIXEL_NUM / 2); i--)
   {
     LEDS.setPixelColor(i, 150, 58, 255); //цвет R, G, B
     if (i <= (PIXEL_NUM - 2))
-      LEDS.setPixelColor(i+1, PALE1_COLOUR); //цвет R, G, B
+      LEDS.setPixelColor(i + 1, PALE1_COLOUR); //цвет R, G, B
     else
     {
       LEDS.setPixelColor(7, PALE1_COLOUR); //цвет R, G, B
       LEDS.setPixelColor(6, PALE2_COLOUR);
     }
     if (i <= (PIXEL_NUM - 3))
-      LEDS.setPixelColor(i+2, PALE2_COLOUR); //цвет R, G, B
+      LEDS.setPixelColor(i + 2, PALE2_COLOUR); //цвет R, G, B
     else
       LEDS.setPixelColor(7, PALE2_COLOUR);
-      
+
     LEDS.show();
     delay(ShowDelay);
     LEDS.setPixelColor(i, BACKGROUND_COLOUR);
     if (i <= (PIXEL_NUM - 2))
-      LEDS.setPixelColor(i+1, BACKGROUND_COLOUR); //цвет R, G, B
+      LEDS.setPixelColor(i + 1, BACKGROUND_COLOUR); //цвет R, G, B
     else
     {
       LEDS.setPixelColor(7, BACKGROUND_COLOUR); //цвет R, G, B
       LEDS.setPixelColor(6, BACKGROUND_COLOUR);
     }
     if (i <= (PIXEL_NUM - 3))
-      LEDS.setPixelColor(i+2, BACKGROUND_COLOUR); //цвет R, G, B
+      LEDS.setPixelColor(i + 2, BACKGROUND_COLOUR); //цвет R, G, B
     else
       LEDS.setPixelColor(7, BACKGROUND_COLOUR);
   }
 }
 //----------------------------------------
-void LEDS_3()
+void flash()
 {
   const unsigned int ShowDelay = 50;//100;
   const unsigned int PauseDelay = 50;//100;
@@ -137,14 +145,14 @@ void LEDS_3()
   int32_t RandomColour;
 
   randomSeed(analogRead(ANALOG_PIN_RANDOM));
-  
+
   for (byte i = 0; i < PIXEL_NUM; i++)
     LEDS.setPixelColor(i, 0, 0, 0);
   LEDS.show();
 
   for (byte j = 0; j < 21; j++)//количество произвольных вспышек 21
   {
-    RandomColour = random(0xFFFFFF);    
+    RandomColour = random(0xFFFFFF);
     RandomPixel = random(PIXEL_NUM);
 
     LEDS.setPixelColor(RandomPixel, RandomColour);
@@ -154,6 +162,14 @@ void LEDS_3()
     LEDS.show();
     delay(PauseDelay);
   }
+
+  //delay(1000);
+}
+
+void snake()
+{
+  const unsigned int ShowDelay = 50;//100;
+  int32_t RandomColour;
 
   RandomColour = random(0xFFFFFF);
 
@@ -184,37 +200,36 @@ void LEDS_3()
     LEDS.show();
     delay(ShowDelay);
   }
-  
-  //delay(1000);
+
 }
 //---------------------------------------
 void LEDS_4()
 {
-  for(uint16_t j=0; j<256*1; j++) //стоит 1 цикл радуги
+  for (uint16_t j = 0; j < 256 * 1; j++) //стоит 1 цикл радуги
+  {
+    for (uint16_t i = 0; i < PIXEL_NUM; i++)
     {
-      for(uint16_t i=0; i< PIXEL_NUM; i++)
-      {
-        LEDS.setPixelColor(i, Wheel(((i * 256 / PIXEL_NUM) + j) & 255));
-      }
-    
-      LEDS.show();
-      delay(3);
+      LEDS.setPixelColor(i, Wheel(((i * 256 / PIXEL_NUM) + j) & 255));
     }
-/*
-    for(byte i=0; i< PIXEL_NUM; i++)
-    {
-      LEDS.setPixelColor(i, 0);
-    }
+
     LEDS.show();
-*/    
+    delay(3);
+  }
+  /*
+      for(byte i=0; i< PIXEL_NUM; i++)
+      {
+        LEDS.setPixelColor(i, 0);
+      }
+      LEDS.show();
+  */
 }
 //----------------------------------------
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
+  if (WheelPos < 85) {
     return LEDS.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
-  if(WheelPos < 170) {
+  if (WheelPos < 170) {
     WheelPos -= 85;
     return LEDS.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
